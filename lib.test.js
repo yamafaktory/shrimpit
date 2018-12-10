e4bf7a5a727bdef15924a09524c0feaf6d7a9c7d
Ben Styles <enquiries@leeds-ebooks.co.uk>
2018-12-10 10:58:30 +0000

    Fix tests by awaiting the returned promise

--- lib.test.js
+++ lib.test.js
@@ -5,12 +5,12 @@ const outputSnapshot = async shrimpit => JSON.parse(await shrimpit.exec())
 
 it('should create a snapshot of the files tree and of the unused exports', async () => {
   const shrimpit = new Shrimpit(inputPaths('test'))
-  expect(outputSnapshot(shrimpit)).toMatchSnapshot()
+  expect(await outputSnapshot(shrimpit)).toMatchSnapshot()
 })
 
 describe('globs', () => {
   it('should handle globs correctly', async () => {
     const shrimpit = new Shrimpit(inputPaths('test/**/*.js', '!**/(c|d)/*'))
-    expect(outputSnapshot(shrimpit)).toMatchSnapshot()
+    expect(await outputSnapshot(shrimpit)).toMatchSnapshot()
   })
 })
