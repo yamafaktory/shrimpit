d15aa046ef93d69cff10f5f4d108396812810936
Davy Duperron <yamafaktory@gmail.com>
2019-02-22 09:24:06 +0100

    Add tests, do some cleanup ♻️.

--- lib.test.js
+++ lib.test.js
@@ -3,14 +3,21 @@ const Shrimpit = require('./lib')
 const inputPaths = (...arr) => ['', '', ...arr, '--json']
 const outputSnapshot = async shrimpit => JSON.parse(await shrimpit.exec())
 
-it('should create a snapshot of the files tree and of the unused exports', async () => {
-  const shrimpit = new Shrimpit(inputPaths('test'))
-  expect(await outputSnapshot(shrimpit)).toMatchSnapshot()
+describe('core functionality', () => {
+  it('should create a snapshot of the files tree and of the unused exports - main', async () => {
+    const shrimpit = new Shrimpit(inputPaths('test/core'))
+    expect(await outputSnapshot(shrimpit)).toMatchSnapshot()
+  })
+
+  it('should create a snapshot of the files tree and of the unused exports - re-exports', async () => {
+    const shrimpit = new Shrimpit(inputPaths('test/re-exports'))
+    expect(await outputSnapshot(shrimpit)).toMatchSnapshot()
+  })
 })
 
-describe('globs', () => {
+describe('globs handling', () => {
   it('should handle globs correctly', async () => {
-    const shrimpit = new Shrimpit(inputPaths('test/**/*.js', '!**/(c|d)/*'))
+    const shrimpit = new Shrimpit(inputPaths('test/core/**/*.js', '!**/(c|d)/*'))
     expect(await outputSnapshot(shrimpit)).toMatchSnapshot()
   })
 })
